a67d91ed0a2945b846e73064a1c057127ad4ca0c
ParkVitna <bbn960903@gmail.com>
2021-02-22 21:23:40 +0900

    2월22일 숙제 (6장 확인문제 - ~15번까지)

---
+++ src/ch06homework/Exercise/Member.java
@@ -0,0 +1,19 @@
+package ch06homework.Exercise;
+
+public class Member {
+	//13번, 14번 확인문제
+	//필드
+	String name;
+	String id;
+	String password;
+	int age;
+	
+	//생성자
+	Member(String name, String id){
+		this.name = name;
+		this.id = id;
+	}
+	
+	//메소드
+
+}
