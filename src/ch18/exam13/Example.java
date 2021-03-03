d71dde5aeb83be9ac987162ef42f83a5b70542fa
ParkVitna <bbn960903@gmail.com>
2021-03-03 21:18:26 +0900

    3월3일 복습1

--- src/ch18/exam13/Example.java
+++ src/ch18/exam13/Example.java
@@ -12,7 +12,7 @@ public class Example {
 		String id = console.readLine();
 		
 		System.out.print("비밀번호: ");
-		String password = new String(console.readPassword());
+		String password = new String(console.readPassword()); //****
 		//char(console.readPassword()-스캐너에서는 사용할 수 없음)->String으로 바꿈
 		
 		System.out.println(id);
